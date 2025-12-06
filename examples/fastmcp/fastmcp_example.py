6ee3c73083d152b2aca390e36f49dd63c57acd00
justint-rl <justint1004@gmail.com>
2025-12-06 03:29:43 -0500

    Change name

--- examples/fastmcp/fastmcp_example.py
+++ examples/fastmcp/fastmcp_example.py
@@ -1,13 +1,13 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP(name="fastmcp_example")
+mcp = FastMCP(name="math")
 
 @mcp.tool
 def add(a: int, b: int) -> int:
   return a + b
 
 def run_mcp_server():
-  mcp.run(transport="http")
+  mcp.run(transport="http", port=8001)
 
 if __name__ == "__main__":
   run_mcp_server()
